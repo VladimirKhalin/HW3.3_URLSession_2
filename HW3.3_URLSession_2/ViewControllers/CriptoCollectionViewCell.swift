f719f1432335bee437bbb85c65b4d385f411d556
VK <art.nebar@mail.ru>
2022-12-18 16:15:32 +0300

    Добавлена  CollectionView

---
+++ HW3.3_URLSession_2/ViewControllers/CriptoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CriptoCollectionViewCell.swift
+//  HW3.3_URLSession_2
+//
+//  Created by Vladimir Khalin on 18.12.2022.
+//
+
+import UIKit
+
+class CriptoCollectionViewCell: UICollectionViewCell {
+    
+}
