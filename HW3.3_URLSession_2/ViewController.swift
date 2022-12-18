d8574580b0509f1213ca17e0468748c212a44b88
VK <art.nebar@mail.ru>
2022-12-18 14:16:56 +0300

    Initial Commit

---
+++ HW3.3_URLSession_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3.3_URLSession_2
+//
+//  Created by Vladimir Khalin on 18.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
